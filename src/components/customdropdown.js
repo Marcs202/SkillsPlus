ccefe1c3b19229adc58baba8dfc15dab0f8b5905
williamre574 <williamreyes082@gmail.com>
2023-10-22 17:31:33 -0600

    api de categorias

--- src/components/customdropdown.js
+++ src/components/customdropdown.js
@@ -1,31 +1,41 @@
-import { SelectList } from 'react-native-dropdown-select-list'
-import React from 'react';
-import { Text,View, StyleSheet } from 'react-native';
+import { SelectList } from "react-native-dropdown-select-list";
+import React, { useEffect, useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 const CustomDropdown = () => {
+  const [selected, setSelected] = useState("");
+  const [apiData, setApiData] = useState([]); // Estado para almacenar los datos de la API
 
-  const [selected, setSelected] = React.useState("");
-  
-  const data = [
-      {key:'1', value:'Mobiles'},
-      {key:'2', value:'Appliances'},
-      {key:'3', value:'Cameras'},
-      {key:'4', value:'Computers'},
-      {key:'5', value:'Vegetables'},
-      {key:'6', value:'Diary Products'},
-      {key:'7', value:'Drinks'},
-  ]
+  // Simula una solicitud a la API, reemplaza esto con tu lógica real de solicitud a la API.
+  useEffect(() => {
+    fetch("http://140.84.176.85:3000/categorias/")
+      .then((response) => response.json())
+      .then((data) => setApiData(data))
+      .catch((error) => console.error(error));
+  }, []);
 
-  return(
-    <SelectList 
-        setSelected={(val) => setSelected(val)} 
-        data={data} 
+  const selectData = apiData.map((item) => ({ key: item.ID, value: item.NOMBRE }));
+
+  return (
+    <>
+    <View style={{ flexDirection: 'row', marginTop:10}}>
+      <SelectList
+        setSelected={(val) => setSelected(val)}
+        data={selectData}
         save="value"
-        boxStyles={{backgroundColor: 'white', marginTop: 30}}
-        dropdownStyles={{backgroundColor: 'white'}}
-    />
-  )
-
+        boxStyles={{  backgroundColor: "white", width: '50%', justifyContent:'space-between' }}
+        dropdownStyles={{ backgroundColor: "white",  }}
+      />
+      <SelectList
+        setSelected={(val) => setSelected(val)}
+        data={selectData}
+        save="value"
+        boxStyles={{  backgroundColor: "white",  width: '50%', justifyContent:'space-between'  }}
+        dropdownStyles={{ backgroundColor: "white"  }}
+      />
+      </View>
+    </>
+  );
 };
 
 export default CustomDropdown;
