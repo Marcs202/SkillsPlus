5fda865f48338c4a6ff9d74750f99a89ac66fd42
Jeff.Portillo <mixjeff@hotmail.com>
2023-11-11 14:46:37 -0600

    hace falta las noti

--- src/MainContainer.jsx
+++ src/MainContainer.jsx
@@ -1,78 +1,89 @@
-import * as React from 'react';
-import { useState } from 'react'
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';import { createStackNavigator } from '@react-navigation/stack';
+import * as React from "react";
+import { useState } from "react";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
 
 //Pantallas
-import HomeScreen from './Screens/HomeSC';
-import DetailScreen from './Screens/DetailSC';
-import ProfileScreen from './Screens/ProfileSC';
-import NotificationScreen from './Screens/NotificationSC';
-import AddServices from './Screens/agregarservicios';
-import Signin from './Screens/Login/signin';
+import HomeScreen from "./Screens/HomeSC";
+import DetailScreen from "./Screens/DetailSC";
+import ProfileScreen from "./Screens/ProfileSC";
+import NotificationScreen from "./Screens/NotificationSC";
+import AddServices from "./Screens/agregarservicios";
+import Signin from "./Screens/Login/signin";
 //Nombre de las pantallas
 const homeName = "Inicio";
 const detailsName = "Servicios";
-const notificationName = "Notificacion";
+const notificationName = "Notificaciones";
 const profileName = "Perfil";
-
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
-function BTab(){
-    return(
-            <Tab.Navigator initialRouteName={homeName} screenOptions={({route})=> ({
-                tabBarIcon:({focused, color, size}) =>{
-                    let iconName;
-                    let rn =route.name;
-                    if (rn === homeName){
-                        iconName = focused ? 'home':'home-outline'
-                    } else if(rn === detailsName){
-                        iconName = focused ? 'list' : 'list-outline'
-                    }else if (rn === profileName){
-                        iconName = focused ? 'person' : 'person-outline'
-                    }else if (rn === notificationName){
-                        iconName = focused ? 'notifications' : 'notifications-outline'
-                        
-                    }
-                    return <Ionicons name={iconName} size={size} color={color}/>
-                },
-                "tabBarActiveTintColor": "#6F2C8C",
-                "tabBarInactiveTintColor": "#BFACC8",
-                "tabBarLabelStyle": {
-                    "paddingBottom": 10,
-                    "fontSize": 10
-                },
-                "tabBarStyle": [
-                    {
-                    "padding": 10,
-                    "height" : 70
-                    },
-                    null
-                ]
-            }) }
-            
-            >
-                <Tab.Screen name='homeName' component={HomeScreen}></Tab.Screen>
-                <Tab.Screen name={detailsName} component={DetailScreen}></Tab.Screen>
-                <Tab.Screen name={notificationName} component={NotificationScreen}></Tab.Screen>
-                <Tab.Screen name={profileName} component={ProfileScreen}></Tab.Screen>
-            </Tab.Navigator>
-    )
+function BTab() {
+  return (
+    <Tab.Navigator
+      initialRouteName={homeName}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          let rn = route.name;
+          if (rn === homeName) {
+            iconName = focused ? "home" : "home-outline";
+          } else if (rn === detailsName) {
+            iconName = focused ? "list" : "list-outline";
+          } else if (rn === profileName) {
+            iconName = focused ? "person" : "person-outline";
+          } else if (rn === notificationName) {
+            iconName = focused ? "notifications" : "notifications-outline";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#6F2C8C",
+        tabBarInactiveTintColor: "#BFACC8",
+        tabBarLabelStyle: {
+          paddingBottom: 10,
+          fontSize: 10,
+        },
+        tabBarStyle: [
+          {
+            padding: 10,
+            height: 70,
+          },
+          null,
+        ],
+      })}
+    >
+      <Tab.Screen name="homeName" component={HomeScreen}></Tab.Screen>
+      <Tab.Screen name={detailsName} component={DetailScreen}></Tab.Screen>
+      <Tab.Screen
+        name={notificationName}
+        component={NotificationScreen}
+      ></Tab.Screen>
+      <Tab.Screen name={profileName} component={ProfileScreen}></Tab.Screen>
+    </Tab.Navigator>
+  );
 }
 
-export function MainContainer(){
-    return(
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name='Tab' component={BTab} options={{ headerShown: false }}></Stack.Screen>
-                <Stack.Screen name="agregarservicios" component={AddServices}/>
-                <Stack.Screen name='Tab2' component={BTab} options={{ headerShown: false }}></Stack.Screen>
-                <Stack.Screen name="login" component={Signin}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+export function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Tab"
+          component={BTab}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen name="agregarservicios" component={AddServices} />
+        <Stack.Screen
+          name="Tab2"
+          component={BTab}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen name="login" component={Signin} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- src/Screens/NotificationSC.jsx
+++ src/Screens/NotificationSC.jsx
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import Card from "../components/card";
 
 export default function NotificationScreen({ navigation }) {
-    return (
-        <SafeAreaView style={{ flex: 1, justifyContent: 'center', margin: 30 }}>
-            <Text> Aqui irán notificaciones de Usuario e Profesional</Text>            
-        </SafeAreaView>
-    )
+  return (
+    <>
+      <ScrollView>
+        <View style={styles.container}>
+          <Card
+            title="Contrato"
+            subtitle="Aceptado"
+            difficulty="Baja - Media"
+            description="Aprende GitHub en 3 semanas"
+            buttonLabel="Finalizar"
+            onPress={() => console.log("Empezar")}
+            gradientColors={["#21c8f6", "#637bff"]}
+            icon={<Text>GitHub Icon</Text>} // Reemplaza esto con el icono adecuado
+          />
+          <Card
+            title="Contrato"
+            subtitle="Confirmación"
+            difficulty="Media - Alta"
+            description="Flutter en 1 año"
+            buttonLabel="Empezar"
+            onPress={() => console.log("Empezar")}
+            gradientColors={["#6edcc4", "#1aab8b"]}
+            icon={<Text>Android Icon</Text>} // Reemplaza esto con el icono adecuado
+          />
+          <Card
+            title="Contrato"
+            subtitle="Rechazado"
+            difficulty="Media - Alta"
+            description="Flutter en 1 año"
+            buttonLabel="Empezar"
+            onPress={() => console.log("Empezar")}
+            gradientColors={["#F54949", "#170834"]}
+            icon={<Text>Android Icon</Text>} // Reemplaza esto con el icono adecuado
+          />
+        </View>
+      </ScrollView>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    searchBox: {
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        borderColor: "#C8C6D7",
-        borderWidth: 1,
-        borderRadius: 8,
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+  },
+});
