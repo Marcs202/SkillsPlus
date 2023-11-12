3e51a7f802d7e0ce05a58c64bee6743948ab2255
Jeff.Portillo <mixjeff@hotmail.com>
2023-11-12 15:18:22 -0600

    Cambios en perfil y Noti

--- src/Screens/ProfileSC.jsx
+++ src/Screens/ProfileSC.jsx
@@ -1,15 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image } from "react-native";
-import { NavigationContainer, useNavigation, useFocusEffect } from '@react-navigation/native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+} from "react-native";
+import {
+  NavigationContainer,
+  useNavigation,
+  useFocusEffect,
+} from "@react-navigation/native";
 import { Card, Button } from "react-native-paper";
-import { AntDesign } from '@expo/vector-icons';
-import Modal from 'react-native-modal';
-import CheckBox from 'react-native-checkbox';
-import axios from 'axios';
+import { AntDesign } from "@expo/vector-icons";
+import Modal from "react-native-modal";
+import CheckBox from "react-native-checkbox";
+import axios from "axios";
 import { useGlobal } from "../asset/valuesglobal";
-
-
 
 export default function ProfileScreen({ isAuthenticated, userType }) {
   const navigation = useNavigation();
@@ -20,34 +29,33 @@ export default function ProfileScreen({ isAuthenticated, userType }) {
     setModalVisible(!isModalVisible);
   };
   const handleLogin = () => {
-    navigation.navigate('login', { screen: 'login' });
+    navigation.navigate("login", { screen: "login" });
   };
 
   const handleLogout = () => {
-      try {
-        //Limpiar datos de sesión, como el ID de usuario o información de autenticación.    
-        userIdProfesional.set(null); // Asigna un valor nulo a userIdProfesional
-        userId.set(null); // Asigna un valor nulo a userId
-        // Luego, redirige al usuario a la pantalla de inicio de sesión 
-        navigation.navigate('login', { screen: 'login' });
-      } catch (error) {
-        console.error('Error al cerrar sesión:', error);
+    try {
+      setUserId("");
+      setUserIdProfesional("");
+      if (userId == "" && userIdProfesional == "") {
+        navigation.navigate("login", { screen: "login" });
       }
-    };      
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error);
+    }
+  };
 
   const handleOptionSelect = (option) => {
     setSelectedOption(option);
-    if (option === 'Opción 1') {
+    if (option === "Opción 1") {
       updateDetailScreenVisibility(true);
-      
     } else {
       updateDetailScreenVisibility(false);
     }
     toggleModal();
   };
 
-  const { userIdProfesional } = useGlobal();
-  const { userId } = useGlobal();
+  const { userIdProfesional, setUserIdProfesional } = useGlobal();
+  const { userId, setUserId } = useGlobal();
 
   //Perfil
   const [users, setUsers] = useState([]);
@@ -61,24 +69,38 @@ export default function ProfileScreen({ isAuthenticated, userType }) {
     }, [userId])
   );
 
+  //Perfil Profesional
+  const [profesionals, setProfesionals] = useState([]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      fetch(`http://140.84.176.85:3000/usuarios/usuario?id=${userId}`)
+        .then((response) => response.json())
+        .then((data) => setProfesionals(data))
+        .catch((error) => console.error(error));
+    }, [userId])
+  );
+
   //Servicios
   const [data, setDataApi] = useState([]); // Aquí almacenaremos los datos de la API
 
   useFocusEffect(
     React.useCallback(() => {
       axios
-        .get(`http://140.84.176.85:3000/servicios/?idProfesional=${userIdProfesional}`)
-        .then(response => {
-          if (response.data && typeof response.data === 'object') {
+        .get(
+          `http://140.84.176.85:3000/servicios/?idProfesional=${userIdProfesional}`
+        )
+        .then((response) => {
+          if (response.data && typeof response.data === "object") {
             setDataApi(response.data); // Analizar solo si es un objeto válido
           } else {
-            console.error('Respuesta de la API no es un JSON válido.');
+            console.error("Respuesta de la API no es un JSON válido.");
           }
         })
-        .catch(error => {
-          console.error('Error al obtener datos de la API:', error);
+        .catch((error) => {
+          console.error("Error al obtener datos de la API:", error);
         });
-    }, [userIdProfesional, userId])
+    }, [userIdProfesional])
   );
 
   return (
@@ -92,14 +114,14 @@ export default function ProfileScreen({ isAuthenticated, userType }) {
           color="#6F2C8C"
           onPress={handleLogout}
         />
-        <AntDesign          
+        <AntDesign
           style={{ marginRight: 20 }}
           name="swap"
           type="font-awesome"
           color="#6F2C8C"
           size={30}
           onPress={() => {
-            toggleModal()
+            toggleModal();
           }}
         />
         <AntDesign
@@ -112,46 +134,80 @@ export default function ProfileScreen({ isAuthenticated, userType }) {
         />
 
         <Modal isVisible={isModalVisible}>
-          <View style={{ backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
+          <View
+            style={{
+              backgroundColor: "white",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
             <Text>Selecciona un rol:</Text>
 
             <CheckBox
               label="Profesional"
-              checked={selectedOption === 'Opción 1'}
-              onChange={() => handleOptionSelect('Opción 1')}
+              checked={selectedOption === "Opción 1"}
+              onChange={() => handleOptionSelect("Opción 1")}
             />
             <CheckBox
               label="Contratista"
-              checked={selectedOption === 'Opción 2'}
-              onChange={() => handleOptionSelect('Opción 2')}
+              checked={selectedOption === "Opción 2"}
+              onChange={() => handleOptionSelect("Opción 2")}
             />
             <Button title="Cerrar" onPress={toggleModal} />
           </View>
         </Modal>
       </View>
 
-      <ScrollView >
-        <View style={styles.userContainer}>
-          <Image source={{ uri: users.Foto }} style={styles.perfil} />
-          <View style={styles.userInfo}>
-            <Text style={styles.name}>{users.Nombre} {users.Apellido}</Text>
-            <Text style={styles.email}>{users.Correo}</Text>
-          </View>
+      {userId == null && userIdProfesional == null ? (
+        <View style={styles.containerEmpty}>
+          <Text style={styles.centeredText}>
+            Por favor, inicia sesion dando click en el icono de las 2 personas
+          </Text>
         </View>
-
-
+      ) : userIdProfesional == null ? (
         <ScrollView>
           <View style={styles.containerIn}>
-            {data.map(item => (
+            <View style={styles.ClientBox}>
+              <Text style={styles.title}>
+                {users.Nombre} {users.Apellido}
+              </Text>
+              <View style={{ width: "70%", flexDirection: "row" }}>
+                <Text style={styles.descriptionIn}>{users.Correo}</Text>
+                <Image source={{ uri: users.Foto }} style={styles.serviceImg} />
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      ) : (
+        <ScrollView>
+          <View style={styles.userContainer}>
+            <Image source={{ uri: users.Foto }} style={styles.perfil} />
+            <View style={styles.userInfo}>
+              <Text style={styles.name}>
+                {users.Nombre} {users.Apellido}
+              </Text>
+              <Text style={styles.email}>{users.Correo}</Text>
+            </View>
+          </View>
+
+          <View style={styles.containerIn}>
+            {data.map((item) => (
               <View key={item.ID_Servicio} style={styles.serviceBox}>
                 <Text style={styles.title}>{item.Titulo}</Text>
-                <View style={{ width: '70%', flexDirection: "row" }}>
+                <View style={{ width: "70%", flexDirection: "row" }}>
                   <Text style={styles.descriptionIn}>{item.Descripcion}</Text>
-                  <Image source={{ uri: item.Foto }} style={styles.serviceImg} />
+                  <Image
+                    source={{ uri: item.Foto }}
+                    style={styles.serviceImg}
+                  />
                 </View>
-                <Button mode="contained" style={styles.serviceBtn}
+                <Button
+                  mode="contained"
+                  style={styles.serviceBtn}
                   onPress={() => {
-                    navigation.navigate('Notificacion', { screen: 'Notificacion' });
+                    navigation.navigate("Notificaciones", {
+                      screen: "Notificaciones",
+                    });
                   }}
                 >
                   Contratacion
@@ -160,22 +216,31 @@ export default function ProfileScreen({ isAuthenticated, userType }) {
             ))}
           </View>
         </ScrollView>
-
-      </ScrollView>
-
+      )}
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  containerEmpty: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  centeredText: {
+    fontSize: 30,
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "#6F2C8C",
+  },
   perfiltop: {
     display: "flex",
-    flexDirection: "row"
+    flexDirection: "row",
   },
   userContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     flex: 2,
-    marginLeft: 10
+    marginLeft: 10,
   },
   userInfo: {
     flex: 1,
@@ -185,56 +250,65 @@ const styles = StyleSheet.create({
     width: 80,
     height: 80,
     marginLeft: 15,
-    borderRadius: 50
+    borderRadius: 50,
   },
   name: {
     fontSize: 25,
-    fontWeight: 'bold',
-    color: '#6F2C8C'
+    fontWeight: "bold",
+    color: "#6F2C8C",
   },
   email: {
-    fontSize: 16
+    fontSize: 16,
   },
   containerIn: {
     marginLeft: 15,
-    marginRight: 15
+    marginRight: 15,
   },
   title: {
     fontSize: 22,
-    fontWeight: 'bold',
-    color: '#6F2C8C',
+    fontWeight: "bold",
+    color: "#6F2C8C",
     padding: 5,
-    marginLeft: 100
+    marginLeft: 100,
   },
   descriptionIn: {
-    color: '#4A376A',
-    fontWeight: '500',
+    color: "#4A376A",
+    fontWeight: "500",
     fontSize: 16,
     padding: 15,
-    alignContent: 'center',
-    textAlign: 'justify'
+    alignContent: "center",
+    textAlign: "justify",
   },
   serviceImg: {
     width: 110,
     height: 130,
     borderRadius: 10,
-    marginTop: '10%'
+    marginTop: "10%",
   },
   serviceBox: {
     borderWidth: 2,
-    borderColor: '#AA92CE', /* #722FE3 */
-    borderStyle: 'dotted',
-    borderRadius: 10, /* #D4E6F1 */
+    borderColor: "#AA92CE" /* #722FE3 */,
+    borderStyle: "dotted",
+    borderRadius: 10 /* #D4E6F1 */,
     marginTop: 25,
     paddingBottom: 15,
-    backgroundColor: '#F5EDF9'
+    backgroundColor: "#F5EDF9",
   },
   serviceBtn: {
     borderWidth: 2,
-    borderColor: '#F5EDF9',
+    borderColor: "#F5EDF9",
     borderRadius: 25,
     width: 140,
     marginLeft: 45,
-    backgroundColor: '#722FE3'
-  }
+    backgroundColor: "#722FE3",
+  },
+  ClientBox: {
+    borderWidth: 2,
+    borderColor: "#AA92CE" /* #722FE3 */,
+    borderStyle: "dotted",
+    borderRadius: 10 /* #D4E6F1 */,
+    marginTop: 100,
+    paddingBottom: 20,
+    backgroundColor: "#F5EDF9",
+  },
 });
